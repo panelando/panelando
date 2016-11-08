6ebe769bb389efb435dfe8c2716a99607fbfa5af
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-11-08 10:02:14 -0200

    Add firebase app config

---
+++ app/lib/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyDz2S2iOm2HJ4B5mAtnZATPOoS3BSfY8gY",
+  authDomain: "panelando-ec672.firebaseapp.com",
+  databaseURL: "https://panelando-ec672.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "13711355875"
+}
+
+firebase.initializeApp(config)
+
+export const auth = firebase.auth
+export const database = firebase.database
