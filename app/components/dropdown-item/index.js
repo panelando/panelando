2f6e60afb7aa035a7aa36104450eaefd8b751036
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-11-17 09:16:55 -0200

    Fix dropdown item props

--- app/components/dropdown-item/index.js
+++ app/components/dropdown-item/index.js
@@ -3,7 +3,7 @@ import styles from './styles'
 
 const DropdownItem = (props) => (
   <div className={styles.container}>
-    <img src={props.img} className={styles.image} />
+    <img src={props.image} className={styles.image} />
     <div className={styles.content}>
       <strong>{props.name}</strong>
       <small>10 receitas</small>
