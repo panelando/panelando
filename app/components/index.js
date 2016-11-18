7c6dd43c9acdc9e787e3794428df1812d287d945
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-11-18 09:49:06 -0200

    Wire up firebase data with recipe list

--- app/components/index.js
+++ app/components/index.js
@@ -1,2 +1,3 @@
 export MenuDialog from './menu-dialog'
 export DropdownItem from './dropdown-item'
+export RecipeCard from './recipe-card'
---
+++ app/components/recipe-card/index.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import R from 'ramda'
+
+import {
+  Card,
+  CardActions,
+  CardMedia,
+  CardText,
+  CardTitle,
+  IconButton
+} from 'react-toolbox'
+
+import {
+  TimeIcon,
+  PortionIcon,
+  DifficultyIcon
+} from 'components/icons'
+
+import styles from './styles'
+
+const translateDifficulty = R.cond([
+  [R.equals('very_easy'), R.always('Muito Fácil')],
+  [R.equals('easy'), R.always('Fácil')],
+  [R.equals('average'), R.always('Médio')],
+  [R.equals('hard'), R.always('Difícil')],
+  [R.equals('very_hard'), R.always('Muito Difícil')]
+])
+
+const RecipeCard = (props) => (
+  <Card className={styles.container} key={props.recipe.id}>
+    <CardMedia
+      aspectRatio="wide"
+      image={props.recipe.image}
+      onClick={() => props.onSeeDetails(props.recipe.id)}
+    />
+
+    <CardTitle
+      title={props.recipe.title}
+      onClick={() => props.onSeeDetails(props.recipe.id)}
+    />
+
+    <CardText className={styles.info}>
+      <div>
+        <IconButton><TimeIcon /></IconButton>
+        <span>{props.recipe.time} min</span>
+      </div>
+
+      <div>
+        <IconButton><PortionIcon /></IconButton>
+        <span>{props.recipe.portion} porções</span>
+      </div>
+
+      <div>
+        <IconButton><DifficultyIcon /></IconButton>
+        <span>{translateDifficulty(props.recipe.difficulty)}</span>
+      </div>
+    </CardText>
+
+    <CardTitle
+      avatar={props.recipe.user.photoURL}
+      title={props.recipe.user.displayName}
+      subtitle="Food artisan and disruptive entrepreneur"
+      style={{ borderTop: '1px solid #f5f5f5', borderBottom: '1px solid #f5f5f5' }}
+    />
+
+    <CardActions>
+      <IconButton icon="bookmark_border" onClick={() => props.onBookmark(props.recipe.id)}/>
+
+      {props.canFavorite ? (
+        <IconButton className={styles.favorite} icon="favorite_border" onClick={() => props.onFavorite(props.recipe.id)}/>
+      ) : (
+        <IconButton className={styles.favorite} icon="favorite" onClick={() => props.onUnfavorite(props.recipe.id)}/>
+      )}
+
+      <span>{props.recipe.likes ? props.recipe.likes.length : 0}</span>
+    </CardActions>
+  </Card>
+)
+
+export default RecipeCard
+
